21bd9a16f7675e32c1f951e4dd063ff3afc7f9ef
aritrarpal <aritrarpal@gmail.com>
2026-05-28 11:04:45 +0530

    feat: Create a basic Spring Boot application class named `HelloSpringApplicati

--- hello-spring/src/main/java/com/example/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/com/example/hellospring/HelloSpringApplication.java
@@ -2,24 +2,12 @@ package com.example.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HelloSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloSpringApplication.class, args);
-    }
-
-}
-
-@RestController
-class HelloController {
-
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, Spring!";
     }
 
 }
