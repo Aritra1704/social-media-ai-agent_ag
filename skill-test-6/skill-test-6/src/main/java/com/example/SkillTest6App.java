8523ce8c1f1aa825489ba105d0b56c273edc0722
aritrarpal <aritrarpal@gmail.com>
2026-05-29 16:14:24 +0530

    feat: Create a Java class SkillTest6App in the package com.example that implem

---
+++ skill-test-6/skill-test-6/src/main/java/com/example/SkillTest6App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SkillTest6App implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Skill Test 6 running");
+    }
+}
