399ded21aa20c52e959cb777d767afa4f3b5ee48
aritrarpal <aritrarpal@gmail.com>
2026-05-29 16:23:04 +0530

    feat: Add the `@SpringBootApplication` annotation to the `SkillTest6App` class

--- skill-test-6/src/main/java/com/example/SkillTest6App.java
+++ skill-test-6/src/main/java/com/example/SkillTest6App.java
@@ -6,13 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SkillTest6App implements CommandLineRunner {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SkillTest6App.class, args);
-    }
-
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Skill Test 6 running");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillTest6App.class, args);
     }
 }
