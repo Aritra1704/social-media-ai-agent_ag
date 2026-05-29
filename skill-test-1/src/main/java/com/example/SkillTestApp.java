9b104db2e7c3691438b160e11c93d3225d0e251a
aritrarpal <aritrarpal@gmail.com>
2026-05-29 14:05:42 +0530

    feat: Create a Java class `SkillTestApp` in the package `com.example`. This cl

---
+++ skill-test-1/src/main/java/com/example/SkillTestApp.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillTestApp implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillTestApp.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Skill Test 1 running");
+    }
+}
